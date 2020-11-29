1f1cac686a55b498f8899c9326d92f753f09929c
Andrik-Ts-019 <andrikdlcmartinez@outlook.com>
2020-11-28 22:58:41 -0600

    RoutingComments

--- BeerBackEnd/Controllers/BeersController.cs
+++ BeerBackEnd/Controllers/BeersController.cs
@@ -11,36 +11,49 @@ namespace BeerBackEnd.Controllers
 {
     public class BeersController : ApiController
     {
+        // .../api/beers/getAllBeers
         [HttpGet]
         public List<BeerDTO> getAllBeers()
         {
             return new BeerSC().getAllBeers();
         }
 
+        // .../api/beers/getBeerById?id=#
         [HttpGet]
         public BeerDTO getBeerById(int id)
         {
             return new BeerSC().getAllBeers().Where(w => w.id == id).FirstOrDefault();
         }
 
+        // .../api/beers/getBeerByName?name=#
         [HttpGet]
         public BeerDTO getBeerByName(string name)
         {
             return new BeerSC().getAllBeers().Where(w => w.name == name).FirstOrDefault();
         }
 
+        // .../api/beers/getBeerRandom
         [HttpGet]
-        public BeerDTO getBeerRamdom()
+        public BeerDTO getBeerRandom()
         {
             var beers = new BeerSC().getAllBeers();
             Random r = new Random();
-            return beers[r.Next(1,beers.Count)];
+            return beers[r.Next(0,beers.Count)];
         }
 
+        // .../api/beers/getBeerAttenuation?level=#
         [HttpGet]
         public BeerDTO getBeerAttenuation(int level)
         {
             return new BeerSC().getAllBeers().Where(w => w.attenuation_level == level).FirstOrDefault();
+        }
+
+        [HttpPost]
+        public List<BeerDTO> addBeer([FromBody] BeerDTO newbeer)
+        {
+            var beers = new BeerSC().getAllBeers();
+            beers.Add(newbeer);
+            return new BeerSC().getAllBeers();
         }
     }
 }
